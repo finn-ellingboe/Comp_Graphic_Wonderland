c02c48a7400a2060db70f1b37cccff840b1d7de4
Finn Ellingboe <ellingbf@tcd.ie>
2025-12-26 22:13:39 +0000

    Added skybox and basic camera control

--- wonderland/wonderland_window.cpp
+++ wonderland/wonderland_window.cpp
@@ -5,6 +5,8 @@
 
 #define STB_IMAGE_WRITE_IMPLEMENTATION
 #include <stb/stb_image_write.h>
+#define STB_IMAGE_IMPLEMENTATION
+#include <stb/stb_image.h>
 
 #include <render/shader.h>
 
@@ -12,9 +14,6 @@
 #include <iostream>
 #define _USE_MATH_DEFINES
 #include <math.h>
-
-
-////   Stuff added from lab3
 
 
 static GLFWwindow* window;
@@ -25,12 +24,13 @@ static void key_callback(GLFWwindow* window, int key, int scancode, int action, 
 static void cursor_callback(GLFWwindow* window, double xpos, double ypos);
 
 // OpenGL camera view parameters
-static glm::vec3 eye_center(-278.0f, 273.0f, 800.0f);
-static glm::vec3 lookat(-278.0f, 273.0f, 0.0f);
+static glm::vec3 eye_center(150.0f, 150.0f, 150.0f);
+static glm::vec3 lookat(150.0f, 150.0f, -150.0f);	// start looking at back wall
 static glm::vec3 up(0.0f, 1.0f, 0.0f);
 static float FoV = 45.0f;
-static float zNear = 600.0f;
+static float zNear = 0.1f;
 static float zFar = 1500.0f;
+
 
 // Lighting control 
 const glm::vec3 wave500(0.0f, 255.0f, 146.0f);
@@ -39,45 +39,292 @@ const glm::vec3 wave700(205.0f, 0.0f, 0.0f);
 static glm::vec3 lightIntensity = 5.0f * (8.0f * wave500 + 15.6f * wave600 + 18.4f * wave700);
 static glm::vec3 lightPosition(-275.0f, 500.0f, -275.0f);
 
-/* Start of shadow mapping?
-// Shadow mapping
-static glm::vec3 lightUp(0, 0, 1);
-static int shadowMapWidth = 0;
-static int shadowMapHeight = 0;
+// function for loading textures
+static GLuint LoadTextureTileBox(const char* texture_file_path) {
+	int w, h, channels;
+	stbi_set_flip_vertically_on_load(true); // This flips our texture along its x axis. This means reversing the image for the skybox is not flipped
+	uint8_t* img = stbi_load(texture_file_path, &w, &h, &channels, 3);
+	GLuint texture;
+	glGenTextures(1, &texture);
+	glBindTexture(GL_TEXTURE_2D, texture);
 
-// TODO: set these parameters 
-static float depthFoV = 0.f;
-static float depthNear = 0.f;
-static float depthFar = 0.f;
+	// To tile textures on a box, we set wrapping to repeat
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE); //Changing this to Clamp to Edge because only small gaps in 1 direction
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR);// This or Linear
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR_MIPMAP_LINEAR);
 
-// Helper flag and function to save depth maps for debugging
-static bool saveDepth = false;
-
-// This function retrieves and stores the depth map of the default frame buffer 
-// or a particular frame buffer (indicated by FBO ID) to a PNG image.
-static void saveDepthTexture(GLuint fbo, std::string filename) {
-	int width = shadowMapWidth;
-	int height = shadowMapHeight;
-	if (shadowMapWidth == 0 || shadowMapHeight == 0) {
-		width = windowWidth;
-		height = windowHeight;
+	if (img) {
+		glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, w, h, 0, GL_RGB, GL_UNSIGNED_BYTE, img);
+		glGenerateMipmap(GL_TEXTURE_2D);
 	}
-	int channels = 3;
+	else {
+		std::cout << "Failed to load texture " << texture_file_path << std::endl;
+	}
+	stbi_image_free(img);
 
-	std::vector<float> depth(width * height);
-	glBindFramebuffer(GL_FRAMEBUFFER, fbo);
-	glReadBuffer(GL_DEPTH_COMPONENT);
-	glReadPixels(0, 0, width, height, GL_DEPTH_COMPONENT, GL_FLOAT, depth.data());
-	glBindFramebuffer(GL_FRAMEBUFFER, 0);
-
-	std::vector<unsigned char> img(width * height * 3);
-	for (int i = 0; i < width * height; ++i) img[3 * i] = img[3 * i + 1] = img[3 * i + 2] = depth[i] * 255;
-
-	stbi_write_png(filename.c_str(), width, height, channels, img.data(), width * channels);
+	return texture;
 }
 
-End of shadow mapping?
-*/ 
+struct Skybox {
+	glm::vec3 scale;		// Size of the box in each axis
+
+	GLfloat vertex_buffer_data[72] = {	// Vertex definition for a canonical box
+		// if we swap position of two opposite corners, then rotate 90 degrees to right, it swaps
+		// left - all good
+		//  z,    y,   x   
+		-1.0f, -1.0f, 1.0f,		// top left
+		-1.0f, 1.0f, 1.0f,		// bottom left
+		1.0f, 1.0f, 1.0f,		// bottom right
+		1.0f, -1.0f, 1.0f,		// top right
+
+		// right - All good
+		1.0f, -1.0f, -1.0f,		// bottom right
+		1.0f, 1.0f, -1.0f,		// top right
+		-1.0f, 1.0f, -1.0f,		// top left
+		-1.0f, -1.0f, -1.0f,	// bottom left
+
+		// front - All good
+		-1.0f, -1.0f, -1.0f,	// bottom left
+		-1.0f, 1.0f, -1.0f,		// top left
+		-1.0f, 1.0f, 1.0f,		// top right
+		-1.0f, -1.0f, 1.0f,		// bottom right
+
+		// behind - All good
+		1.0f, -1.0f, 1.0f,		// bottom right
+		1.0f, 1.0f, 1.0f,		// top right
+		1.0f, 1.0f, -1.0f,		// top left
+		1.0f, -1.0f, -1.0f,		// bottom left
+
+		// top - All good
+		-1.0f, 1.0f, -1.0f,		// 
+		1.0f, 1.0f, -1.0f,
+		1.0f, 1.0f, 1.0f,		//
+		-1.0f, 1.0f, 1.0f,
+
+		// bottom - pointing outwards (flip by swapping zs)
+		1.0f, -1.0f, -1.0f,		//
+		-1.0f, -1.0f, -1.0f,
+		-1.0f, -1.0f, 1.0f,		//
+		1.0f, -1.0f, 1.0f,
+	};
+
+	GLfloat color_buffer_data[72] = {
+		// Front, red
+		1.0f, 0.0f, 0.0f,
+		1.0f, 0.0f, 0.0f,
+		1.0f, 0.0f, 0.0f,
+		1.0f, 0.0f, 0.0f,
+
+		// Back, yellow
+		1.0f, 1.0f, 0.0f,
+		1.0f, 1.0f, 0.0f,
+		1.0f, 1.0f, 0.0f,
+		1.0f, 1.0f, 0.0f,
+
+		// Left, green
+		0.0f, 1.0f, 0.0f,
+		0.0f, 1.0f, 0.0f,
+		0.0f, 1.0f, 0.0f,
+		0.0f, 1.0f, 0.0f,
+
+		// Right, cyan
+		0.0f, 1.0f, 1.0f,
+		0.0f, 1.0f, 1.0f,
+		0.0f, 1.0f, 1.0f,
+		0.0f, 1.0f, 1.0f,
+
+		// Top, blue
+		0.0f, 0.0f, 1.0f,
+		0.0f, 0.0f, 1.0f,
+		0.0f, 0.0f, 1.0f,
+		0.0f, 0.0f, 1.0f,
+
+		// Bottom, magenta
+		1.0f, 0.0f, 1.0f,
+		1.0f, 0.0f, 1.0f,
+		1.0f, 0.0f, 1.0f,
+		1.0f, 0.0f, 1.0f,
+	};
+
+	GLuint index_buffer_data[36] = {		// 12 triangle faces of a box
+		0, 1, 2,
+		0, 2, 3,
+
+		4, 5, 6,
+		4, 6, 7,
+
+		8, 9, 10,
+		8, 10, 11,
+
+		12, 13, 14,
+		12, 14, 15,
+
+		16, 17, 18,
+		16, 18, 19,
+
+		20, 21, 22,
+		20, 22, 23,
+	};
+
+
+	// Here I am making everything end 1 before it actually should because I was getting black lines in the skybox
+	GLfloat uv_buffer_data[48] = {
+		// x, y
+		// left
+		0.25f, 0.334f,		// bottom right
+		0.25f, 0.666f,		// top right
+		0.0f, 0.666f,		// top left					BRING BACK
+		0.0f, 0.334f,		// bottom left
+		// right
+		0.75f, 0.334f,
+		0.75f, 0.666f,
+		0.5f, 0.666f,
+		0.5f, 0.334f,
+		// front
+		0.5f, 0.334f,		// bottom right
+		0.5f, 0.666f,		// top right
+		0.25f, 0.666f,		// top left
+		0.25f, 0.334f,		// bottom left
+		// behind
+		1.0f, 0.334f,
+		1.0f, 0.666f,
+		0.75f, 0.666f,
+		0.75f, 0.334f,
+		// Top
+		0.5f, 0.667f,		// 
+		0.5f, 1.0f,			//
+		0.251f, 1.0f,		// X is higher to avoid a black line
+		0.251f, 0.667f,		// 
+		// Bottom
+		0.499f, 0.0f,
+		0.499f, 0.334f,
+		0.25f, 0.334f,
+		0.25f, 0.0f,
+
+	};
+
+	// OpenGL buffers
+	GLuint vertexArrayID;
+	GLuint vertexBufferID;
+	GLuint indexBufferID;
+	GLuint colorBufferID;
+	GLuint uvBufferID;
+	GLuint textureID;
+
+	// Shader variable IDs
+	GLuint mvpMatrixID;
+	GLuint textureSamplerID;
+	GLuint programID;
+
+	void initialize(glm::vec3 scale) {
+		// Define scale of the building geometry
+		this->scale = scale;
+
+		// Create a vertex array object
+		glGenVertexArrays(1, &vertexArrayID);
+		glBindVertexArray(vertexArrayID);
+
+		// Create a vertex buffer object to store the vertex data		
+		glGenBuffers(1, &vertexBufferID);
+		glBindBuffer(GL_ARRAY_BUFFER, vertexBufferID);
+		glBufferData(GL_ARRAY_BUFFER, sizeof(vertex_buffer_data), vertex_buffer_data, GL_STATIC_DRAW);
+
+		// Create a vertex buffer object to store the color data
+		glGenBuffers(1, &colorBufferID);
+		glBindBuffer(GL_ARRAY_BUFFER, colorBufferID);
+		glBufferData(GL_ARRAY_BUFFER, sizeof(color_buffer_data), color_buffer_data, GL_STATIC_DRAW);
+
+		// Create a vertex buffer object to store the UV data
+		glGenBuffers(1, &uvBufferID);
+		glBindBuffer(GL_ARRAY_BUFFER, uvBufferID);
+		glBufferData(GL_ARRAY_BUFFER, sizeof(uv_buffer_data), uv_buffer_data, GL_STATIC_DRAW);
+
+		// Create an index buffer object to store the index data that defines triangle faces
+		glGenBuffers(1, &indexBufferID);
+		glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, indexBufferID);
+		glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(index_buffer_data), index_buffer_data, GL_STATIC_DRAW);
+
+		// Create and compile our GLSL program from the shaders
+		//programID = LoadShadersFromFile("../lab2/box.vert", "../lab2/box.frag");
+		programID = LoadShadersFromFile("C:/Users/finn_/Desktop/College_Programming/Computer_Graphics/lab2/lab2/box.vert",
+			"C:/Users/finn_/Desktop/College_Programming/Computer_Graphics/lab2/lab2/box.frag");
+		if (programID == 0)
+		{
+			std::cerr << "Failed to load shaders." << std::endl;
+		}
+
+		// Get a handle for our "MVP" uniform
+		mvpMatrixID = glGetUniformLocation(programID, "MVP");
+
+		// Load a texture
+		textureID = LoadTextureTileBox("C:/Users/finn_/Desktop/College_Programming/Computer_Graphics/lab2/lab2/studio_garden.png");
+
+		// Get a handle to texture sampler 
+		textureSamplerID = glGetUniformLocation(programID, "textureSampler");
+	}
+
+	void render(glm::mat4 cameraMatrix) {
+		glUseProgram(programID);
+
+		glEnableVertexAttribArray(0);
+		glBindBuffer(GL_ARRAY_BUFFER, vertexBufferID);
+		glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, 0);
+
+		glEnableVertexAttribArray(1);
+		glBindBuffer(GL_ARRAY_BUFFER, colorBufferID);
+		glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 0, 0);
+
+		glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, indexBufferID);
+
+		// Model transform 
+		// -----------------------
+		glm::mat4 modelMatrix = glm::mat4();
+
+		// Scale the box along each axis
+		// TODO : Have the skybox move with the user as they walk around
+		modelMatrix = glm::scale(modelMatrix, scale); // Scale the skybox relative to the global eye position attributes
+		// -----------------------
+
+		// Set model-view-projection matrix
+		glm::mat4 mvp = cameraMatrix * modelMatrix;
+		glUniformMatrix4fv(mvpMatrixID, 1, GL_FALSE, &mvp[0][0]);
+
+
+		// Enable UV buffer and texture sampler
+		glEnableVertexAttribArray(2);
+		glBindBuffer(GL_ARRAY_BUFFER, uvBufferID);
+		glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, 0, 0);
+
+		// Set textureSampler to use texture unit 0
+		glActiveTexture(GL_TEXTURE0);
+		glBindTexture(GL_TEXTURE_2D, textureID);
+		glUniform1i(textureSamplerID, 0);
+
+
+		// Draw the box
+		glDrawElements(
+			GL_TRIANGLES,      // mode
+			36,    			   // number of indices
+			GL_UNSIGNED_INT,   // type
+			(void*)0           // element array buffer offset
+		);
+
+		glDisableVertexAttribArray(0);
+		glDisableVertexAttribArray(1);
+		//glDisableVertexAttribArray(2);
+	}
+
+	void cleanup() {
+		glDeleteBuffers(1, &vertexBufferID);
+		glDeleteBuffers(1, &colorBufferID);
+		glDeleteBuffers(1, &indexBufferID);
+		glDeleteVertexArrays(1, &vertexArrayID);
+		glDeleteBuffers(1, &uvBufferID);
+		glDeleteTextures(1, &textureID);
+		glDeleteProgram(programID);
+	}
+};
 
 struct CornellBox {
 
@@ -210,6 +457,7 @@ struct CornellBox {
 };
 
 
+
 int main(void)
 {
 	// Initialise GLFW
@@ -248,114 +496,12 @@ int main(void)
 		return -1;
 	}
 
-	// Prepare shadow map size for shadow mapping. Usually this is the size of the window itself, but on some platforms like Mac this can be 2x the size of the window. Use glfwGetFramebufferSize to get the shadow map size properly. 
-	// glfwGetFramebufferSize(window, &shadowMapWidth, &shadowMapHeight);
 
 	// Background
 	glClearColor(0.2f, 0.2f, 0.25f, 0.0f);
 
 	glEnable(GL_DEPTH_TEST);
 	glEnable(GL_CULL_FACE);
-
-
-	// initialising all the arrays for our boxes
-	/* Removing the outer box
-	GLfloat outerBoxVertexBufferData[60] = {
-		// Floor 
-		-552.8, 0.0, 0.0,
-		0.0, 0.0,   0.0,
-		0.0, 0.0, -559.2,
-		-549.6, 0.0, -559.2,
-
-		// Ceiling
-		-556.0, 548.8, 0.0,
-		-556.0, 548.8, -559.2,
-		0.0, 548.8, -559.2,
-		0.0, 548.8,   0.0,
-
-		// Left wall 
-		-552.8,   0.0,   0.0,
-		-549.6,   0.0, -559.2,
-		-556.0, 548.8, -559.2,
-		-556.0, 548.8,   0.0,
-
-		// Right wall 
-		0.0,   0.0, -559.2,
-		0.0,   0.0,   0.0,
-		0.0, 548.8,   0.0,
-		0.0, 548.8, -559.2,
-
-		// Back wall 
-		-549.6,   0.0, -559.2,
-		0.0,   0.0, -559.2,
-		0.0, 548.8, -559.2,
-		-556.0, 548.8, -559.2
-	};
-
-	GLfloat outerBoxNormalBufferData[60] = {
-		// Floor 
-		0.0, 1.0, 0.0,
-		0.0, 1.0, 0.0,
-		0.0, 1.0, 0.0,
-		0.0, 1.0, 0.0,
-
-		// Ceiling
-		0.0, -1.0, 0.0,
-		0.0, -1.0, 0.0,
-		0.0, -1.0, 0.0,
-		0.0, -1.0, 0.0,
-
-		// Left wall 
-		1.0, 0.0, 0.0,
-		1.0, 0.0, 0.0,
-		1.0, 0.0, 0.0,
-		1.0, 0.0, 0.0,
-
-		// Right wall 
-		-1.0, 0.0, 0.0,
-		-1.0, 0.0, 0.0,
-		-1.0, 0.0, 0.0,
-		-1.0, 0.0, 0.0,
-
-		// Back wall 
-		0.0, 0.0, 1.0,
-		0.0, 0.0, 1.0,
-		0.0, 0.0, 1.0,
-		0.0, 0.0, 1.0
-	};
-
-	GLfloat outerBoxColorBufferData[60] = {
-		// Floor
-		1.0f, 1.0f, 1.0f,
-		1.0f, 1.0f, 1.0f,
-		1.0f, 1.0f, 1.0f,
-		1.0f, 1.0f, 1.0f,
-
-		// Ceiling
-		1.0f, 1.0f, 1.0f,
-		1.0f, 1.0f, 1.0f,
-		1.0f, 1.0f, 1.0f,
-		1.0f, 1.0f, 1.0f,
-
-		// Left wall
-		1.0f, 0.0f, 0.0f,
-		1.0f, 0.0f, 0.0f,
-		1.0f, 0.0f, 0.0f,
-		1.0f, 0.0f, 0.0f,
-
-		// Right wall
-		0.0f, 1.0f, 0.0f,
-		0.0f, 1.0f, 0.0f,
-		0.0f, 1.0f, 0.0f,
-		0.0f, 1.0f, 0.0f,
-
-		// Back wall
-		1.0f, 1.0f, 1.0f,
-		1.0f, 1.0f, 1.0f,
-		1.0f, 1.0f, 1.0f,
-		1.0f, 1.0f, 1.0f
-	};
-	*/
 
 
 	GLfloat tallBoxVertexBufferData[60] = {
@@ -499,11 +645,12 @@ int main(void)
 		1.0f, 1.0f, 1.0f
 	};
 
-	// Create the classical Cornell Box
-	//CornellBox b
-	CornellBox tallBox, smallBox;
-	//b.initialize(outerBoxVertexBufferData, outerBoxNormalBufferData, outerBoxColorBufferData);
 
+	Skybox skybox;
+	skybox.initialize(glm::vec3(500, 500, 500));  // Scale x,y,z
+
+
+	CornellBox tallBox, smallBox;
 	//Box tallBox, smallBox;
 	tallBox.initialize(tallBoxVertexBufferData, tallBoxNormalBufferData, otherBoxColorBufferData);
 	smallBox.initialize(smallBoxVertexBufferData, smallBoxNormalBufferData, otherBoxColorBufferData);
@@ -520,18 +667,11 @@ int main(void)
 		viewMatrix = glm::lookAt(eye_center, lookat, up);
 		glm::mat4 vp = projectionMatrix * viewMatrix;
 
-		//b.render(vp);
+		skybox.render(vp);
+
 		tallBox.render(vp);
 		smallBox.render(vp);
 
-		/* Shadows
-		if (saveDepth) {
-			std::string filename = "depth_camera.png";
-			saveDepthTexture(0, filename);
-			std::cout << "Depth texture saved to " << filename << std::endl;
-			saveDepth = false;
-		}
-		*/
 
 		// Swap buffers
 		glfwSwapBuffers(window);
@@ -541,9 +681,11 @@ int main(void)
 	while (!glfwWindowShouldClose(window));
 
 	// Clean up
-	//b.cleanup();
+	skybox.cleanup();
+
 	tallBox.cleanup();
 	smallBox.cleanup();
+
 	// Close OpenGL window and terminate GLFW
 	glfwTerminate();
 
@@ -554,39 +696,55 @@ static void key_callback(GLFWwindow* window, int key, int scancode, int action, 
 {
 	if (key == GLFW_KEY_R && action == GLFW_PRESS)
 	{
-		eye_center = glm::vec3(-278.0f, 273.0f, 800.0f);
-		lightPosition = glm::vec3(-275.0f, 500.0f, -275.0f);
+		eye_center = glm::vec3(150.0f, 150.0f, 150.0f);
+		lookat = glm::vec3(150.0f, 150.0f, -150.0f);
 
 	}
 
-	if (key == GLFW_KEY_UP && (action == GLFW_REPEAT || action == GLFW_PRESS))
-	{
-		eye_center.y += 20.0f;
-	}
-
-	if (key == GLFW_KEY_DOWN && (action == GLFW_REPEAT || action == GLFW_PRESS))
-	{
-		eye_center.y -= 20.0f;
-	}
-
-	if (key == GLFW_KEY_LEFT && (action == GLFW_REPEAT || action == GLFW_PRESS))
-	{
-		eye_center.x -= 20.0f;
-	}
-
-	if (key == GLFW_KEY_RIGHT && (action == GLFW_REPEAT || action == GLFW_PRESS))
-	{
-		eye_center.x += 20.0f;
-	}
+	// change where the skybox is centred relative to the person
 
 	if (key == GLFW_KEY_W && (action == GLFW_REPEAT || action == GLFW_PRESS))
 	{
-		lightPosition.z -= 20.0f;
+		eye_center.z -= 20.0f;
+		// skybox.x -= 20.0f    // Need to make skybox variables global
+	}
+
+	if (key == GLFW_KEY_A && (action == GLFW_REPEAT || action == GLFW_PRESS))
+	{
+		eye_center.x -= 20.0f;
+		lookat.x -= 20.0f;
 	}
 
 	if (key == GLFW_KEY_S && (action == GLFW_REPEAT || action == GLFW_PRESS))
 	{
-		lightPosition.z += 20.0f;
+		eye_center.z += 20.0f;
+	}
+
+	if (key == GLFW_KEY_D && (action == GLFW_REPEAT || action == GLFW_PRESS))
+	{
+		eye_center.x += 20.0f;
+		lookat.x += 20.0f;
+	}
+
+
+	if (key == GLFW_KEY_UP && (action == GLFW_REPEAT || action == GLFW_PRESS))
+	{
+		lookat.y += 20.0f;
+	}
+
+	if (key == GLFW_KEY_DOWN && (action == GLFW_REPEAT || action == GLFW_PRESS))
+	{
+		lookat.y -= 20.0f;
+	}
+
+	if (key == GLFW_KEY_LEFT && (action == GLFW_REPEAT || action == GLFW_PRESS))
+	{
+		lookat.x -= 20.0f;
+	}
+
+	if (key == GLFW_KEY_RIGHT && (action == GLFW_REPEAT || action == GLFW_PRESS))
+	{
+		lookat.x += 20.0f;
 	}
 
 	/*
@@ -600,7 +758,9 @@ static void key_callback(GLFWwindow* window, int key, int scancode, int action, 
 		glfwSetWindowShouldClose(window, GL_TRUE);
 }
 
+
 static void cursor_callback(GLFWwindow* window, double xpos, double ypos) {
+	/*
 	if (xpos < 0 || xpos >= windowWidth || ypos < 0 || ypos > windowHeight)
 		return;
 
@@ -617,9 +777,8 @@ static void cursor_callback(GLFWwindow* window, double xpos, double ypos) {
 	lightPosition.y = y * scale + 278;
 
 	//std::cout << lightPosition.x << " " << lightPosition.y << " " << lightPosition.z << std::endl;
+	*/
 }
-
-
 
 
 //  For infinite scrollablility just have the skybox be centered on wherever the person is
